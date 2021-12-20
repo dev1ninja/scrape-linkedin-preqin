75e906d63c48c0a5764a1f605d9189fedbcca43a
dev1ninja <devninja.up@gmail.com>
2021-12-21 03:35:44 +0900

    update scraper

--- src/helpers/pageScraper.js
+++ src/helpers/pageScraper.js
@@ -11,20 +11,28 @@ const scraperObject = {
         
         await page.waitForSelector('.normal');
 
-        console.log('load finished')
+        console.log('first proxy page load finished')
 
         let link = "http://hdl.library.upenn.edu/1017/90974";
         let mainPage = await browser.newPage();
         await mainPage.goto(link);
 
         await mainPage.waitForSelector('#login-div');
-        console.log("load finished");
+        console.log("login page load finished");
 
         await mainPage.type('#pennname', USER_NAME);
         await mainPage.type('#password', PASSWORD);
 
         await mainPage.click('.btn');
 
+        await mainPage.waitForSelector('#ctl00_ctl00_preqin');
+
+        await mainPage.click('#ctl00_ctl00_btnGoToPro');
+        await mainPage.waitForSelector('.dashboard__grid');
+
+        await mainPage.goto('https://pro-preqin-com.proxy.library.upenn.edu/discover/investors');
+        await mainPage.waitForSelector('.defaultInvestor');
+        console.log("Search result loaded")
         // await browser.close();
         //#penntoken
     }
